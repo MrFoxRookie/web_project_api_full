525bdf7ad042310b9b31b3e2cf15821e132678d2
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2025-11-17 15:28:25 -0600

    Paso 1 completado

---
+++ backend/models/text.tx
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String, minlength: 2, maxlength: 30, required: true
+  },
+  about: {
+    type: String, minlength: 2, maxlength: 30, required: true
+  },
+ avatar: {
+    type: String,
+    required: [true, 'El enlace del avatar es obligatorio'],
+    validate: {
+      validator: function(v) {
+        return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/.test(v);
+      },
+      message: props => `${props.value} no es una URL valida`
+    }
+  }
+});
+
+module.exports = mongoose.model('user', userSchema);
+
+
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,25 +1,47 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   name: {
-    type: String, minlength: 2, maxlength: 30, required: true
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
   },
   about: {
-    type: String, minlength: 2, maxlength: 30, required: true
-  },
- avatar: {
     type: String,
-    required: [true, 'El enlace del avatar es obligatorio'],
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: [true, "El enlace del avatar es obligatorio"],
     validate: {
-      validator: function(v) {
-        return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/.test(v);
+      validator: function (v) {
+        return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/.test(
+          v
+        );
       },
-      message: props => `${props.value} no es una URL valida`
-    }
-  }
+      message: (props) => `${props.value} no es una URL valida`,
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return validator.isEmail(v);
+      },
+      message: (props) => `${props.value} no es un email valido`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    // select: false,
+  },
 });
 
-module.exports = mongoose.model('user', userSchema);
-
-
+module.exports = mongoose.model("user", userSchema);
