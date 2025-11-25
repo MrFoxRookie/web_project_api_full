bd7868e2605ac274e297802cb37351e434b1588f
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2025-11-25 15:43:35 -0600

    Se agregarón validadores de solicitud a cards.js

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,11 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getAllCards, createCard, removeCard, likeCard, dislikeCard} = require('../controllers/cards');
+const {
+  getAllCards,
+  createCard,
+  removeCard,
+  likeCard,
+  dislikeCard,
+} = require("../controllers/cards");
+const { celebrate, Joi } = require("celebrate");
+const validator = require("validator");
 
-router.get('/cards', getAllCards);
-router.post('/cards', createCard);
-router.delete('/cards/:cardId', removeCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+router.get("/cards", getAllCards);
+
+router.post(
+  "/cards",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).required(),
+      link: Joi.string().required().custom(validateURL),
+    }),
+  }),
+  createCard
+);
+
+router.delete(
+  "/cards/:cardId",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24).required(),
+    }),
+  }),
+  removeCard
+);
+
+router.put(
+  "/cards/:cardId/likes",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24).required(),
+    }),
+  }),
+  likeCard
+);
+
+router.delete(
+  "/cards/:cardId/likes",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24).required(),
+    }),
+  }),
+  dislikeCard
+);
 
 module.exports = router;
