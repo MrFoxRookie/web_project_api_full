1dc8b0defaa41b00f8e823c03756eb3a7fd1d7fd
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2025-12-01 16:38:31 -0600

    Se empieza de 0

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,13 @@ mongoose.connect("mongodb://localhost:27017/aroundb", {
   useUnifiedTopology: true,
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
